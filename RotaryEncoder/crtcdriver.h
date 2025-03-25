fd9ffe75db7dcf2892a2b39b6c9562e481f8c366
Bratski <bfm.zonderland@gmail.com>
2025-03-25 08:49:42 +0100

    added rotary driver

---
+++ RotaryEncoder/crtcdriver.h
@@ -0,0 +1,45 @@
+#ifndef RTCDRIVER_H
+#define RTCDRIVER_H
+
+#pragma once
+#include <gpiod.h>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+
+class CRtcdriver
+{
+public:
+    CRtcdriver() = default;
+    CRtcdriver(const unsigned int &SWITCH, const unsigned int &CLK, const unsigned int &DT);
+    ~CRtcdriver();
+
+    bool initialize();
+    void stop() { _running = false; }
+    void run();
+
+    const int &getCounter() { return _rotaryCounter; }
+    bool getSwitchState() { return _switchState; }
+
+    void setPins(const unsigned int &SWITCH, const unsigned int &CLK, const unsigned int &DT);
+    void setRotaryCounter(int &counter) { _rotaryCounter = counter; }
+
+    const unsigned int &getPinSWITCH() { return _pin1; }
+    const unsigned int &getPinCLK() { return _pin2; }
+    const unsigned int &getPinDT() { return _pin3; }
+
+private:
+    unsigned int _pin1; // SWITCH
+    unsigned int _pin2; // CLK
+    unsigned int _pin3; // DT
+    int _rotaryCounter = 0;
+    const char *_chipname = "gpiochip0";
+    gpiod_line *_line1;
+    gpiod_line *_line2;
+    gpiod_line *_line3;
+    gpiod_chip *_chip;
+    bool _running;
+    bool _switchState;
+};
+
+#endif
